8253300e35c2cab3ac63845bece44b896cffd13e
zahra <99148531+zahrasadat80@users.noreply.github.com>
2024-05-06 23:13:26 +0330

    build the grid and when you mouseover color changes

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+let row;
+let column;
 
 for (let i=1; i <=16 ; i++){
-    let row = document.createElement('div');
+    row = document.createElement('div');
     row.classList.add('row');
     container.appendChild(row);
   for (let j=1 ; j <=16 ; j++){
-    let column = document.createElement('div');
+    column = document.createElement('div');
     column.classList.add('column');
     row.appendChild(column);
+    column.addEventListener('mouseover',function(){
+        this.classList.add('click')
+    })
  }
 }
 
+
+
+// let squaresCount = null;
+// button.addEventListener('click',function(){
+//    squaresCount = Number(prompt('how many squares do you want per side? '));
+//    if (squaresCount =! null){
+//     container.innerHTML='';
+//     for (let i=1; i <=squaresCount ; i++){
+//         row = document.createElement('div');
+//         row.classList.add('row');
+//         container.appendChild(row);
+//       for (let j=1 ; j <=squaresCount ; j++){
+//         column = document.createElement('div');
+//         column.classList.add('column');
+//         row.appendChild(column);
+//      }
+//     }
+    
+//    }
+// })
